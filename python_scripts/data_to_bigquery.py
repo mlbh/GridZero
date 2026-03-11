2e92de8b491c1ee01259600e3f380ec362f81be6
Madeleine Howe <m.libertybella@gmail.com>
2026-03-11 13:25:13 +0000

    rain and precipitation added to weather data

--- python_scripts/data_to_bigquery.py
+++ python_scripts/data_to_bigquery.py
@@ -1,19 +1,6 @@
-# Code to load a df into big query
-
-PROJECT = 'gridzero-489711'
-DATASET ='historical_weather'
-TABLE = '02_full_params_weather_2025'
-
-table = f'{PROJECT}.{DATASET}.{TABLE}'
-
-client = bigquery.Client(project=PROJECT)
-
-write_mode = 'WRITE_TRUNCATE'
-job_config = bigquery.LoadJobConfig(write_disposition=write_mode)
-
-job = client.load_table_from_dataframe(weather_a_df, table, job_config=job_config)
-result = job.result()
-
+# Contents:
+# 1 - function to upload a df to big query
+# 2 - fucntion to load df from big query
 
 def upload_to_bigquery(df: pd.DataFrame,PROJECT: str,DATASET: str,TABLE: str, write_mode: str = "WRITE_TRUNCATE"):
     '''
@@ -37,11 +24,4 @@ def upload_to_bigquery(df: pd.DataFrame,PROJECT: str,DATASET: str,TABLE: str, wr
     return f'{TABLE}, {len(df):,} rows to {table_ref}'
 
 
-# Example
-# upload_to_bigquery(
-#     df=weather_a_df,
-#     project='gridzero-489711',
-#     dataset='historical_weather',
-#     table='02_full_params_weather_2025'
-# )
-# 
+def load_from_bigquery():
