7c40b7d5fb2dd5b2a399b72e88cf7887a5d34df9
Madeleine Howe <m.libertybella@gmail.com>
2026-03-11 14:21:41 +0000

    fucntion to upload/load data to BQ

--- python_scripts/data_to_bigquery.py
+++ python_scripts/data_to_bigquery.py
@@ -1,10 +1,14 @@
 # Contents:
+# 0 - imports needed
 # 1 - function to upload a df to big query
 # 2 - fucntion to load df from big query
 
+from google.cloud import bigquery
+import pandas as pd
+
 def upload_to_bigquery(df: pd.DataFrame,PROJECT: str,DATASET: str,TABLE: str, write_mode: str = "WRITE_TRUNCATE"):
     '''
-    Upload a DataFrame to a BigQuery table.
+    Upload a pd DataFrame to a BigQuery table.
     Function arguments:
         DF: DataFrame to upload
         PROJECT: GCP project ID
@@ -24,4 +28,21 @@ def upload_to_bigquery(df: pd.DataFrame,PROJECT: str,DATASET: str,TABLE: str, wr
     return f'{TABLE}, {len(df):,} rows to {table_ref}'
 
 
-def load_from_bigquery():
+def load_from_bigquery(PROJECT: str, DATASET: str, TABLE: str):
+    ''' Load data from BigQuery into a pd DataFrame.
+    Arguments:
+        PROJECT: GCP project ID
+        DATASET: BigQuery dataset name
+        TABLE: BigQuery table name
+    Returns: pandas DataFrame '''
+    
+    client = bigquery.Client(project=PROJECT)
+    query = f'''
+    SELECT *
+    FROM `{PROJECT}.{DATASET}.{TABLE}`
+    '''
+    df = client.query(query).to_dataframe()
+    rows, cols = df.shape
+    print(f"Loaded {rows:,} rows and {cols} columns from {PROJECT}.{DATASET}.{TABLE}")
+
+    return df
