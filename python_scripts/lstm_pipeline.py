6b7cbd4fca356774e6c86012e9f45ee2c6d5d8cf
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jamesjosh.la04@gmail.com>
2026-03-12 17:52:04 +0000

    new scripts

---
+++ python_scripts/lstm_pipeline.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+
