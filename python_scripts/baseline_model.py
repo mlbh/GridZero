8806bec556ebd4d9f6244e386ad6ddec0148a6dc
Madeleine Howe <m.libertybella@gmail.com>
2026-03-12 10:17:16 +0000

    baseline model checkpoint, comparision of models

--- python_scripts/baseline_model.py
+++ python_scripts/baseline_model.py
@@ -2,6 +2,8 @@ from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, r2_score
 from xgboost import XGBRegressor
+
+# python script imports
 from data_to_bigquery import load_from_bigquery
 from feature_engineering import engineer_features, drop_lag_nulls
 
@@ -9,6 +11,8 @@ from feature_engineering import engineer_features, drop_lag_nulls
 df = load_from_bigquery('gridzero-489711', 'merged_set', 'Fully_merged_dataset_2025')
 
 # feature engineering of the df
+# note that there is a bug if 'target_col='carbon_instensity' arg is explicity included in the function call
+# target_col = 'carbon_intensity' is set to default
 df = engineer_features(df, target_col='carbon_intensity')
 validate_features(df)
 df = drop_lag_nulls(df)
@@ -17,7 +21,7 @@ df = drop_lag_nulls(df)
 feature_cols = [
     'temperature_2m_c', 'wind_speed_100m_ms', 'wind_gusts_10m_ms',
     'cloud_cover_pct', 'shortwave_radiation_wm2', 'direct_radiation_wm2',
-    'diffuse_radiation_wm2', 'pressure_msl_hpa', 'snowfall_cm',
+    'diffuse_radiation_wm2', 'pressure_msl_hpa', 'snowfall_cm', 'rain_mm', 'precipitation_mm',
     'Fossil Gas', 'Nuclear', 'Wind Offshore', 'Wind Onshore',
     'Solar', 'Biomass', 'TotalOutput-MW',
     'lag_48', 'lag_336', 'hour', 'month', 'is_weekend'
@@ -28,28 +32,21 @@ target_col = 'carbon_intensity'
 X = df[feature_cols]
 y = df[target_col]
 
-# ── TEMPORAL SPLIT ───────────────────────────────────────────────────
+# split for single year (baseline)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30)
+
+#
+
+# split for full dataset (ensure minimum of 2 years for test)
 split_idx = int(len(df) * 0.8)
 X_train, X_test = X.iloc[:split_idx], X.iloc[split_idx:]
 y_train, y_test = y.iloc[:split_idx], y.iloc[split_idx:]
 
-# ── TRAIN ────────────────────────────────────────────────────────────
-model = make_pipeline(
-    XGBRegressor(n_estimators=100, learning_rate=0.1, max_depth=6, random_state=42)
-)
-model.fit(X_train, y_train)
+# check split ouputs
+X_train.shape, X_test.shape, y_train.shape, y_test.shape
 
-# ── EVALUATE ─────────────────────────────────────────────────────────
-y_pred = model.predict(X_test)
-print(f"MAE: {mean_absolute_error(y_test, y_pred):.2f} gCO2/kWh")
-print(f"R²:  {r2_score(y_test, y_pred):.4f}")
+# train
 
-# ── PLOT ─────────────────────────────────────────────────────────────
-import matplotlib.pyplot as plt
-plt.figure(figsize=(15,4))
-plt.plot(y_test.values[:200], label='Actual', alpha=0.7)
-plt.plot(y_pred[:200], label='Predicted', alpha=0.7)
-plt.title('Carbon Intensity — Actual vs Predicted')
-plt.ylabel('gCO2/kWh')
-plt.legend()
-plt.show()
+# evaluate
+
+# plot
