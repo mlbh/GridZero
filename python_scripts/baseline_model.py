9c63c87526f3ebe2e5c4626dd4a8b3dac0c98fef
Madeleine Howe <m.libertybella@gmail.com>
2026-03-13 16:15:40 +0000

    xgb baseline model script fucntions finished and model opti started

--- python_scripts/baseline_model.py
+++ python_scripts/baseline_model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, mean_absolute_error, max_error
+from sklearn.metrics import root_mean_squared_error, mean_absolute_error, r2_score, mean_absolute_error, max_error
 from xgboost import XGBRegressor
 from sklearn.preprocessing import StandardScaler
 
@@ -10,7 +10,7 @@ from data_to_bigquery import load_from_bigquery
 from feature_engineering import engineer_features, drop_lag_nulls, validate_features
 
 
-# baseline_preproc fucntion that runs if features need to be engineered
+# xgb training preproc fucntion that runs if features need to be engineered
 # imports Daniels feature_engineering to engineer cols if not present
 # py scripts listed in from/imports
 # drop datetime later in model step, not here
@@ -23,13 +23,10 @@ def xgb_train_preproc(
     Apply feature engineering ONLY if engineered columns are missing.
     Otherwise return df unchanged (except optional 'time' to 'datetime' rename).
     '''
-
     df = df.copy()
-
     # ensure datetime col name if needed
     if 'time' in df.columns and 'datetime' not in df.columns:
         df = df.rename(columns={'time': 'datetime'})
-
      # engineer features only if missing
     required_features = [
         'lag_48',
@@ -40,35 +37,36 @@ def xgb_train_preproc(
         'is_weekend'
     ]
 
+    # if year = TRUE
     if add_year_lag:
         required_features.append('lag_17520')
-
+    # embedded function fuzzy match
+    # suffix matcher if features renamed
+    def feature_exists(df_cols, feature):
+        return any(col.endswith(feature) for col in df_cols)
     # only engineer features if ANY required features missing
-    if not all(col in df.columns for col in required_features):
+    if not all(feature_exists(df.columns, col) for col in required_features):
         df = engineer_features(df, target_col=target_col, add_year_lag=add_year_lag)
-
-    # # drop rows where target missing
-    # df = df.dropna(subset=[target_col])
-
-    # # drop lag null rows
-    # lag_cols = [col for col in ['lag_48', 'lag_336', 'lag_17520'] if col in df.columns]
-    # if lag_cols:
-    #     df = df.dropna(subset=lag_cols)
-
     # simple drop null to make sure can parse saucey tables and raw tables
     df = df.dropna()
-
     return df
+
 
 # Function to predict with baseline model simple/non-temporal split
 # imports load_from bigquery fucntion to load df
+# outputs: model, X_train, X_test, y_train, y_test = baseline_model_
 def baseline_model_xgb(
                         PROJECT: str='gridzero-489711',
                         DATASET: str='merged_set',
                         TABLE: str='test_merge_2017_onward_raw',
                         target_col='carbon_intensity_gCO2_kWh',
                         test_size:int = 0.3
-                        ) -> XGBRegressor:
+                        ) -> tuple[XGBRegressor,
+                                   pd.DataFrame,
+                                   pd.DataFrame,
+                                   pd.DataFrame,
+                                   pd.DataFrame,
+                                   ]:
 
     # load df from BQ
     df = load_from_bigquery(PROJECT=PROJECT, DATASET=DATASET, TABLE=TABLE)
@@ -88,13 +86,8 @@ def baseline_model_xgb(
     X = df.drop(columns=[target_col, 'datetime'], errors='ignore').copy()
     # drop all non-numeric cols
     X = X.select_dtypes(include='number')
-
-    ''' Debugging line, uncomment if errores relating to dtypes'''
-    # print("NON NUMERIC:", X.select_dtypes(exclude='number').columns.tolist())
-
     # converting from Int64 (if applicable)so can also use the sauncey merged data tables
     X = X.astype(float)
-
     y = df[target_col]
 
     # test train split for single year
@@ -111,67 +104,52 @@ def baseline_model_xgb(
     # training model
     model.fit(X_train, y_train)
 
-    return model, X.columns.tolist()
+    return model, X_train, X_test, y_train, y_test
+
+# follow with this
+# feature_cols = X_train.columns.tolist()
+
+# trained model evaluation fucntion
+def evaluate_trained_model(model, X_test, y_test) -> dict:
+    y_pred = model.predict(X_test)
+
+    return {
+        'MAE': mean_absolute_error(y_test, y_pred),
+        'RMSE': root_mean_squared_error(y_test, y_pred),
+        'R2': r2_score(y_test, y_pred),
+        'MaxError': max_error(y_test, y_pred)
+    }
 
 # preping prediction features to be used with frontend
 # Not needed atm but might be needed if new stream of data added
-def prepare_prediction_features(
-                        df_new: pd.DataFrame,
-                        feature_cols: list,
-                        target_col: str='carbon_intensity_gCO2_kWh'
-                    ) -> pd.DataFrame:
-
-    # rename if col 'time' is present
-    df_new = df_new.rename(columns={'time': 'datetime'}).copy()
-    df_new = baseline_preproc(df_new, target_col=target_col)
+def pred_preproc_xgb(df_new: pd.DataFrame,
+                    feature_cols: list,
+                    target_col: str='carbon_intensity_gCO2_kWh'
+                ) -> pd.DataFrame:
 
     X_new = df_new.drop(columns=[target_col, 'datetime'], errors='ignore').copy()
     X_new = X_new.select_dtypes(include='number')
     X_new = X_new.astype(float)
-
-    # keep only training columns
-    X_new = X_new.reindex(columns=feature_cols, fill_value=0)
+    X_new = X_new.reindex(columns=feature_cols)
 
     return X_new
 
-
-def make_prediction(
+# predictio function
+def xgb_prediction(
             model,
             df_new: pd.DataFrame,
             feature_cols: list,
             target_col: str='carbon_intensity_gCO2_kWh'
-        ):
-
+            ):
 # calls fancy frontend function
-    X_new = prepare_prediction_features(
-    df_new=df_new,
-    feature_cols=feature_cols,
-    target_col=target_col
-)
-
+    X_new = pred_preproc_xgb(
+            df_new=df_new,
+            feature_cols=feature_cols,
+            target_col=target_col
+        )
+    # predicting
     y_pred = model.predict(X_new)
-
     return y_pred
-
-
-#the idea was to end up with something like this...
-def make_prediction(X_new):
-    baseline_model = baseline_model_xgb()
-
-    y_pred = baseline_model.predict(X_new)
-    return y_pred
-
-def evaluate_model(model, X_test, y_test) -> dict:
-    y_pred = model.predict(X_test)
-
-    return {
-        'model': model_name,
-        'model_fit_t': cv_results['fit_time'].mean(),
-        'mae_mean': -cv_results['test_mae'].mean(),
-        'rmse_mean': -cv_results['test_rmse'].mean(),
-        'r2_mean': cv_results['test_r2'].mean(),
-        'max_err_max': -cv_results['test_max_err'].max(),
-    }
 
 
 
