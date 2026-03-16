031896ea949383a41e5eeb260cd9c76e5e1a704b
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jamesjosh.la04@gmail.com>
2026-03-16 23:10:40 +0000

    training and saving models to GCS

---
+++ python_scripts/fast_test.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from fastapi import FastAPI
+
+app = FastAPI()
+# app.state.model = registry.load_model()
+
+
+
+# http://127.0.0.1:8000/predict?pickup_datetime=2014-07-06+19:18:00&pickup_longitude=-73.950655&pickup_latitude=40.783282&dropoff_longitude=-73.984365&dropoff_latitude=40.769802&passenger_count=2
+@app.get("/predict")
+def predict(
+        pickup_datetime: str,  # 2014-07-06 19:18:00
+        pickup_longitude: float,    # -73.950655
+        pickup_latitude: float,     # 40.783282
+        dropoff_longitude: float,   # -73.984365
+        dropoff_latitude: float,    # 40.769802
+        passenger_count: int
+    ):      # 1
+    """
+    Make a single course prediction.
+    Assumes `pickup_datetime` is provided as a string by the user in "%Y-%m-%d %H:%M:%S" format
+    Assumes `pickup_datetime` implicitly refers to the "US/Eastern" timezone (as any user in New York City would naturally write)
+    """
+    X = pd.DataFrame([
+        {'pickup_datetime': pd.Timestamp(pickup_datetime,tz="US/Eastern"),
+        'pickup_longitude': pickup_longitude,
+        'pickup_latitude': pickup_latitude,
+        'dropoff_longitude': dropoff_longitude,
+        'dropoff_latitude': dropoff_latitude,
+        'passenger_count': passenger_count}
+        ])
+
+    processed_X = preprocessor.preprocess_features(X)
+
+    model = app.state.model
+
+    fare = model.predict(processed_X)
+    print(fare)
+    print(type(fare))
+    return {'fare': float(fare)}
