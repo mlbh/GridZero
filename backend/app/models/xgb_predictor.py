91192f6e328c21b65f8f8ddeff2bfbb254f8efed
Madeleine Howe <m.libertybella@gmail.com>
2026-03-18 17:08:06 +0000

    xgb feature builder

--- backend/app/models/xgb_predictor.py
+++ backend/app/models/xgb_predictor.py
@@ -6,7 +6,20 @@ class XGBPredictor:
     def __init__(self, model):
         self.model = model
 
-    def predict(self, features):
+    def predict(self, features_df):
+
+        """
+        features_df: 48 rows of weather + generation features (no lags yet)
+        Returns: list of 48 carbon intensity predictions
+        """
+
+        predictions = []
+
+        for i in range (len(features_df)):
+            row = features_df.iloc[i],copy()
+
+        
+
 
         predictions = self.model.predict(features)
 
